f94a2be552cf9cd156a4344f6ecb4e6594418753
Shanmugapriyan <shanmugapriyan9696@gmail.com>
2023-01-15 20:55:26 +0530

    Coursera tasks

---
+++ src/main/java/com/data/structures/algorithms/coursera/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.data.structures.algorithms.coursera.helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/com/data/structures/algorithms/sorting/Combination.java
@@ -0,0 +1,25 @@
+package com.data.structures.algorithms.sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combination {
+
+    private int recursiveCheck(Integer[] arr, int targetValue, int index, List<Integer> result) {
+
+
+        return 0;
+    }
+
+    public int checkCombination(Integer[] arr, int targetValue) {
+
+        List<List<Integer>> results = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            List<Integer> individualResult = new ArrayList<>();
+            for (int j = i + 1; j < arr.length; j++) {
+            }
+        }
+
+        return 0;
+    }
+}
